35279eae963d4716379fa8eb4b653d74624157cb
Ravindran Dharshan <dharshanravindran8@gmail.com>
2025-10-21 09:05:43 +0530

    feat: Add TypeScript definitions for Asgardeo environment variables

---
+++ ReactNativeAppAuthCli/src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+  export const ASGARDEO_ISSUER: string;
+  export const ASGARDEO_CLIENT_ID: string;
+  export const ASGARDEO_REDIRECT_URL: string;
+  export const ASGARDEO_POST_LOGOUT_REDIRECT_URL: string;
+}
